b884e370f19cf6f2b3cda233c9638a0eac35802c
roymas100 <amir.dantas@hotmail.com>
2023-06-08 19:43:52 -0300

    wip/menu

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -31,8 +31,9 @@ const menuItems: MenuItemProps[] = [{
 ]
 
 const Menu: React.FC = () => {
-  return <Flex w="285px" h="100vh" bgColor='#FFFFFF' flexDir='column' alignItems="center" pt='42px' pb='42px'>
-    <Flex gap={8} alignItems='center'>
+  return (
+  <Flex w="285px" h="100vh" bgColor='#FFFFFF' flexDir='column' pt='42px' pb='42px' >
+    <Flex gap={8} alignItems='center' pl='60px'>
         <Avatar src='./avatar.svg' height={60} w={60}/>
 
         <Flex flexDir='column'>
@@ -41,16 +42,17 @@ const Menu: React.FC = () => {
         </Flex>
     </Flex>
 
-    <Flex flexDir='column' w="100%" pt="16px" pb="16px" gap="16px">
+    <Flex flexDir='column' w="100%" pt="32px" pb="16px" gap="16px">
         {menuItems.map(item => <MenuItem key={item.text} svg={item.svg} text={item.text} active={item.text === 'Dashboard'}/>)}
     </Flex>
 
-  </Flex>
+  </Flex>)
 }
 
 const MenuItem: React.FC<MenuItemProps> = ({ svg, text, active }) => {
 return ( 
-    <Flex position="relative" gap="8px" w="100%" height="36px" pl="60px" alignItems="center" opacity={active ? 1 : 0.5}>
+    <Flex position="relative" gap="8px" w="100%" height="36px" pl="60px" alignItems="center" opacity={active ? 1 : 0.5} _hover={{
+    }}>
         <Image src={svg} alt={text} boxSize="24px"></Image>
         <Text fontWeight={500} fontSize={14} lineHeight="21px"color='#000'>{text}</Text>
         {active && <Flex position="absolute" left="-10px" height="36px" width="19px" borderRadius="16px" backgroundColor="#E5D405"></Flex>}
