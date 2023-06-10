e15e5d1bfda02d81698e35cfe209ed8ba4ca624e
roymas100 <amir.dantas@hotmail.com>
2023-06-10 08:47:40 -0300

    wip

--- src/components/SchoolCard/index.tsx
+++ src/components/SchoolCard/index.tsx
@@ -5,12 +5,15 @@ import GoalBar from '../GoalBar';
 // import { Container } from './styles';
 
 interface SchoolCardProps {
-    name: string
+    name: string;
+    onClick?: React.MouseEventHandler<HTMLDivElement> | undefined
 }
 
-const SchoolCard: React.FC<SchoolCardProps> = ({ name }) => {
-  return <Card width="322px" >
-<CardHeader height="94px" bg="gray" borderTopRadius="8px" />
+const SchoolCard: React.FC<SchoolCardProps> = ({ name, onClick }) => {
+  return <Card onClick={onClick} width="322px" cursor="pointer" borderStyle="solid" borderWidth="2px" borderColor="gray"  _hover={{
+    borderColor: "#000"
+  }} >
+<CardHeader height="94px" bg="gray" borderTopRadius="4px" />
 
 <CardBody padding="24px" paddingBottom="36px" display="flex" flexDir="column" gap="24px">
     <Text>
